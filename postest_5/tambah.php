a62d186c67ebe48d57087cac9c15fc2902bd9e47
Avinezi24 <99675958+Avinezi24@users.noreply.github.com>
2022-10-20 23:59:09 +0800

    Add files via upload

---
+++ postest_5/tambah.php
@@ -0,0 +1,50 @@
+<?php 
+    require "konek.php";
+    ob_start();
+    require "list.php";
+    ob_end_clean();
+
+
+    $sql = mysqli_query($konek,"SELECT *FROM jadwal");
+    $data=mysqli_fetch_array($sql);
+
+    if(isset($_POST["tambah"])){
+        mysqli_query($konek,"INSERT INTO jadwal set 
+        judul ='$_POST[judul]',
+        kategori ='$_POST[kategori]',
+        tanggal ='$_POST[tanggal]'");
+        echo "
+        <script> alert('data telah diubah')</script>";
+        echo "<meta http-equiv=refresh content=1;URL='list.php'>";}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <td>JUDUL</td>
+                <td><input type="text" name="judul"> </td>
+            </tr>
+            <tr>
+                <td>KATEGORI</td>
+                <td><input type="text" name="kategori">"> </td>
+            </tr>
+            <tr>
+                <td>TANGGAL</td>
+                <td><input type="date" name="tanggal">> </td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="tambah" value="simpan"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
